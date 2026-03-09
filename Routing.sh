bede32d50b47f940a662d2925d4e6904846e1ae1
Kenton Lee <klee0078@uni.sydney.edu.au>
2026-03-10 01:28:07 +1100

    docs(scaffold): Add required Routing.sh file as per specification

---
+++ Routing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Put the command to run your program here.
+# For example, if you wish to use Python:
+python3 main.py "$@"
+
+# Remember that `"$@"` passes the command-line arguments passed
+# to `Routing.sh` directly to your script.
