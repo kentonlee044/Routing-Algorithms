27a31857188e17b367a37ec54342acc2caf8b08c
Kenton Lee <klee0078@uni.sydney.edu.au>
2026-03-14 01:25:51 +1100

    feat(node): functions to create a socket, connect to neighbours, accept incoming connection attempts and updating the flow of the program

--- threads/listener.py
+++ threads/listener.py
@@ -1,4 +1,5 @@
 import threading
+import node
 
 class listener(threading.Thread):
     
@@ -8,18 +9,22 @@ class listener(threading.Thread):
 
     '''
     Thread should be running over this function listening for input and calling handle_update() before calling forward_update() to send the update packets to the routing calculations thread
+    - Needs to call node's function to listen for updates from the neighbours and update the graph accordingly
+    - also needs to call node's functions to 
+    - needs to call accept_neighbours()
     '''
     def run(self):
         print("Listener thread started successfully.")
+        # TODO - call node's accept_neighbours() function to accept incoming connections from neighbours and test it
 
     '''
     sends the formatted command to the routing thread
     '''
-    def handle_update():
+    def handle_update(self):
         pass
 
     '''
     Parses the command from STDIN into a ready and usable format
     '''
-    def read_command():
+    def read_command(self):
         pass
